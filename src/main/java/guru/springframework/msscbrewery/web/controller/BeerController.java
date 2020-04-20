0cbfae7dea869dcd2d1c7c0f6b3f936ac8fc20dc
divine777 <tijanailic933@gmail.com>
2020-04-20 15:25:14 +0200

    changed services and controllers

--- src/main/java/guru/springframework/msscbrewery/web/controller/BeerController.java
+++ src/main/java/guru/springframework/msscbrewery/web/controller/BeerController.java
@@ -30,12 +30,18 @@ public class BeerController {
 
 
     @PostMapping
-    public ResponseEntity handlePost(BeerDto beerDto){
+    public ResponseEntity handlePost(@RequestBody  BeerDto beerDto){
         BeerDto savedDto = beerService.saveNewBeer(beerDto);
         HttpHeaders headers = new HttpHeaders();
         // todo add hostname to url
         headers.add("Location","/api/v1/beer/" + savedDto.getId().toString());
         return new ResponseEntity(headers,HttpStatus.CREATED);
+    }
+
+    @PutMapping({"/{beerId}"})
+    public ResponseEntity handleUpdate(@PathVariable("beerId") UUID beerId,BeerDto beerDto){
+        beerService.updateBeer(beerId,beerDto);
+        return  new ResponseEntity(HttpStatus.NO_CONTENT);
     }
 
 }
