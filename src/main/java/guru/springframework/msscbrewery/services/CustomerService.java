4bb9f9e645b26ac2a1eb8945e2a4be0c2be3302d
divine777 <tijanailic933@gmail.com>
2020-04-20 15:52:22 +0200

    updated Customer service

--- src/main/java/guru/springframework/msscbrewery/services/CustomerService.java
+++ src/main/java/guru/springframework/msscbrewery/services/CustomerService.java
@@ -8,4 +8,10 @@ import guru.springframework.msscbrewery.web.model.CustomerDto;
 public interface CustomerService {
 
     CustomerDto getCustomerById(UUID beerId);
+
+    CustomerDto saveNewCustomer(CustomerDto customerDto);
+
+    void updateCustomer(UUID customerId, CustomerDto customerDto);
+
+    void deleteCustomerById(UUID customerId);
 }
--- src/main/java/guru/springframework/msscbrewery/services/CustomerServiceImpl.java
+++ src/main/java/guru/springframework/msscbrewery/services/CustomerServiceImpl.java
@@ -1,11 +1,13 @@
 package guru.springframework.msscbrewery.services;
 
 
+import guru.springframework.msscbrewery.web.model.BeerDto;
 import guru.springframework.msscbrewery.web.model.CustomerDto;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.UUID;
-
+@Slf4j
 @Service
 public class CustomerServiceImpl implements CustomerService{
     @Override
@@ -14,4 +16,28 @@ public class CustomerServiceImpl implements CustomerService{
                 .customerName("Tijana Ilic")
                 .build();
     }
+
+    @Override
+    public CustomerDto saveNewCustomer(CustomerDto customerDto) {
+        return CustomerDto.builder()
+                .id(UUID.randomUUID())
+                .build();
+    }
+
+    @Override
+    public void updateCustomer(UUID customerId, CustomerDto customerDto) {
+        // todo impl- would add a real impl to update customer
+    }
+
+    @Override
+    public void deleteCustomerById(UUID customerId) {
+    log.debug("Deleting a  customer ...");
+    }
+
+
+
+
+
+
+
 }
